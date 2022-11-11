44258bbd2b824aa53d371d71999124c15edf45f4
Neil Voss <vossman77@yahoo.com>
2022-11-11 17:31:37 -0600

    add read_minifigIDs_from_file()

--- libbrick.py
+++ libbrick.py
@@ -20,7 +20,6 @@ def _flatten_dict_gen(d, parent_key, sep):
 def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str = '.'):
 	return dict(_flatten_dict_gen(d, parent_key, sep))
 
-
 #============================
 #============================
 def make_timestamp():
@@ -37,13 +36,16 @@ def make_timestamp():
 
 #============================
 #============================
-def read_setIDs_from_file(setIDFile):
+def read_setIDs_from_file(setIDFile, remove_dups=False):
 	if not os.path.isfile(setIDFile):
 		return None
 	setIDs = []
 	f = open(setIDFile, "r")
 	for line in f:
 		sline = line.strip()
+		if '\t' in sline:
+			bits = sline.split('\t')
+			sline = bits[0].strip()
 		if sline.startswith('#'):
 			continue
 		elif re.search('^[0-9]+$', sline):
@@ -53,10 +55,42 @@ def read_setIDs_from_file(setIDFile):
 			setID = sline
 		else:
 			print("???", sline)
+			time.sleep(2)
 			continue
 		setIDs.append(setID)
 	f.close()
-	setIDs = list(set(setIDs))
+	### remove duplicates
+	if remove_dups is True:
+		setIDs = list(set(setIDs))
 	setIDs.sort()
 	print("Found {0} set IDs to process".format(len(setIDs)))
 	return setIDs
+
+#============================
+#============================
+def read_minifigIDs_from_file(minifigIDFile, remove_dups=False):
+	if not os.path.isfile(minifigIDFile):
+		return None
+	minifigIDs = []
+	f = open(minifigIDFile, "r")
+	for line in f:
+		sline = line.strip()
+		if '\t' in sline:
+			bits = sline.split('\t')
+			sline = bits[0].strip()
+		if sline.startswith('#'):
+			continue
+		elif re.search('^[a-z0-9]+[0-9]{3,}[a-z]?[0-9]?$', sline):
+			minifigID = sline
+		else:
+			print("???", sline)
+			time.sleep(2)
+			continue
+		minifigIDs.append(minifigID)
+	f.close()
+	### remove duplicates
+	if remove_dups is True:
+		minifigIDs = list(set(setIDs))
+	minifigIDs.sort()
+	print("Found {0} minifig IDs to process".format(len(minifigIDs)))
+	return minifigIDs
