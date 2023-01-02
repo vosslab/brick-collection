11aa8e8f2ab3552fede22beff45fb6e5de7e5994
Neil Voss <vossman77@yahoo.com>
2023-01-01 22:59:30 -0600

    fixes

--- lookupMinifig-Bricklink.py
+++ lookupMinifig-Bricklink.py
@@ -3,6 +3,8 @@
 import os
 import sys
 import html
+import time
+import random
 
 import libbrick
 import bricklink_wrapper
@@ -18,7 +20,7 @@ if __name__ == '__main__':
 		print("usage: ./lookupLego.py <csv txt file with lego IDs>")
 		sys.exit(1)
 
-	minifigIDs = libbrick.read_minifigIDs_from_file(minifigIDFile)
+	minifigIDpairs = libbrick.read_minifigIDpairs_from_file(minifigIDFile)
 
 	timestamp = libbrick.make_timestamp()
 	csvfile = "minifig_data-bricklink-{0}.csv".format(timestamp)
@@ -26,12 +28,21 @@ if __name__ == '__main__':
 	f = open(csvfile, "w")
 	BLwrap = bricklink_wrapper.BrickLink()
 	line = 0
-	for minifigID in minifigIDs:
+	for pair in minifigIDpairs:
+		minifigID, setID = pair
 		line += 1
 		sys.stderr.write(".")
-		minifig_data = BLwrap.getMinifigData(minifigID)
-		category_name = BLwrap.getCategoryNameFromMinifigID(minifigID)
+		try:
+			minifig_data = BLwrap.getMinifigData(minifigID)
+		except LookupError:
+			continue
+		try:
+			category_name = BLwrap.getCategoryNameFromMinifigID(minifigID)
+		except LookupError:
+			time.sleep(random.random())
+			category_name = None
 		minifig_data['category_name'] = category_name
+		minifig_data['set_id'] = setID
 		price_data = BLwrap.getMinifigsPriceData(minifigID)
 		total_data = {**minifig_data, **price_data}
 		total_data['minifig_id'] = minifigID
@@ -46,11 +57,16 @@ if __name__ == '__main__':
 			try:
 				value = total_data[key]
 				if isinstance(value, str):
+					value = value.replace('\n', ' ')
+					value = value.replace('\t', ' ')
 					value = value.replace(',', ' ')
 					value = value.replace('  ', ' ')
 					if '&' in value:
 						value = html.unescape(value)
-				f.write("%s\t"%(str(value)))
+				if value is None:
+					f.write("\t")
+				else:
+					f.write("%s\t"%(str(value)))
 			except KeyError:
 				print("missing key: "+key)
 				f.write("\t")
